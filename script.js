e2e7a2e40307497f6ba4c5a2e65203e52fb82146
Manash Dutta <manash_dutta@msn.com>
2024-10-22 21:17:37 +0530

    add about up section

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
+const navItem = document.querySelector(".nav-link");
 
 menu.addEventListener("pointerdown", () => {
   navbar.classList.toggle("change");
   menu.classList.toggle("change");
 });
- 
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    navbar.classList.remove("change");
+    menu.classList.remove("change");
+  }
+});
+
+document.addEventListener("pointerdown", (event) => {
+  // Check if the clicked element is not the menu or nav-link
+  if (!navItem.contains(event.target) && !menu.contains(event.target)) {
+    navbar.classList.remove("change");
+    menu.classList.remove("change");
+  }
+});
