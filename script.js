fd05eb1bb1d16564435acc50a2e77a3dea9afda2
Manash Dutta <manash_dutta@msn.com>
2024-10-24 15:08:05 +0530

    work on swiper.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Navbar
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
 const navItem = document.querySelector(".nav-link");
+import Swiper from "swiper";
+import "swiper/css";
 
 menu.addEventListener("pointerdown", () => {
   navbar.classList.toggle("change");
@@ -14,10 +17,60 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-document.addEventListener("pointerdown", (event) => {
+document.addEventListener("pointerdown", (e) => {
   // Check if the clicked element is not the menu or nav-link
-  if (!navItem.contains(event.target) && !menu.contains(event.target)) {
+  // !navItem.contains(e.target) &&
+  if (!menu.contains(e.target)) {
     navbar.classList.remove("change");
     menu.classList.remove("change");
   }
+});
+
+// About Us Section
+const video = document.querySelector(".video");
+const playPauseBtn = document.querySelector(".buttons i");
+const videoBar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    playPauseBtn.className = "far fa-pause-circle";
+    video.style.opacity = 0.7;
+  } else {
+    video.pause();
+    playPauseBtn.className = "far fa-play-circle";
+    video.style.opacity = 0.3;
+  }
+};
+
+playPauseBtn.addEventListener("pointerdown", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  const barWidth = video.currentTime / video.duration;
+  videoBar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    playPauseBtn.className = "far fa-play-circle";
+    video.style.opacity = 0.3;
+  }
+});
+
+// Pricing Section
+// Swiper.js
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
